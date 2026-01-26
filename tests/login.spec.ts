7b5a84703f790265e3ba717ddac6b122ee5f7ab4
Natalia Camero Carreño <nataliacameroc@gmail.com>
2026-01-26 14:27:26 +0100

    chore: config path aliases and update roadmap

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { LoginPage } from "../pages/LoginPage";
+import { LoginPage } from "@pages/LoginPage";
 
 test("TC-01: Login Exitoso (Happy Path)", async ({ page }) => {
   //Instanciamos la clase LoginPage
