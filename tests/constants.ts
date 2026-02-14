d22c93bbffdf46aba9b0e2ce8557c2a6ca23e39c
Natalia Camero Carreño <nataliacameroc@gmail.com>
2026-02-14 16:50:10 +0100

    refactor: Add test constants for authentication and product data

---
+++ tests/constants.ts
@@ -0,0 +1,29 @@
+export const VALID_CREDENTIALS: {
+  usuario: string;
+  contraseña: string;
+} = {
+  usuario: "standard_user",
+  contraseña: "secret_sauce",
+};
+
+export const INVALID_CREDENTIALS: {
+  usuario: string;
+  contraseña: string;
+} = {
+  usuario: "standard_user",
+  contraseña: "secret_sauce",
+};
+
+export const TITLE_PRODUCT_PAGE: string = "Products";
+
+export const MAIN_LIST: string[] = [
+  "Sauce Labs Backpack",
+  "Sauce Labs Bike Light",
+  "Sauce Labs Bolt T-Shirt",
+  "Sauce Labs Fleece Jacket",
+  "Sauce Labs Onesie",
+  "Test.allTheThings() T-Shirt (Red)",
+];
+export const PRODUCT_NAME: string = "Sauce Labs Backpack";
+
+export const TEXT_TO_HAVE_ONE_PRODUCT: string = "1";
