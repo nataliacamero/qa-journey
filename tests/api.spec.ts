3a47c4445122321615ba0e45bd9b2512d06bd076
Natalia Camero Carreño <nataliacameroc@gmail.com>
2026-04-06 17:07:50 +0200

    feat: refactor pruebas de API para utilizar UserController en lugar de solicitudes directas

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from "@playwright/test";
+import { UserController } from "controllers/UserControllers";
 
 interface IResponseBodyJSONPlaceHolder {
   name: string;
@@ -31,8 +32,10 @@ test.describe("Pruebas del backend", () => {
   test("TC-016: Validar respuesta GET de lista de usuarios (API)", async ({
     request,
   }) => {
-    const response = await request.get("/users");
+    // Instanciamos la clase UserContller para usar sus métodos.
+    const userController = new UserController(request);
 
+    const response = await userController.getUsers();
     expect(response.status()).toBe(200);
 
     const responseBody: IResponseBodyUser[] = await response.json();
@@ -44,13 +47,17 @@ test.describe("Pruebas del backend", () => {
   test("Validar creación de usuario mediante POST (API)", async ({
     request,
   }) => {
+    // Instanciamos la clase UserContller para usar sus métodos.
+    const userController = new UserController(request);
+
     // Preparamos los datos que usaremos al servidor
     const newUserData = {
       name: "Mateito Amado",
       job: "Senior SDTE",
     };
 
-    const response = await request.post("/users", { data: newUserData });
+    const response = await userController.createUser(newUserData);
+
     // Validadmos el status code de creacion del registro o recurso 201.
     expect(response.status()).toBe(201);
 
@@ -65,13 +72,18 @@ test.describe("Pruebas del backend", () => {
   test("TC-18: Validar actualización de usuario mediante PUT (API)", async ({
     request,
   }) => {
+    // Instanciamos la clase UserContller para usar sus métodos.
+    const userController = new UserController(request);
+
     // Preparamos los datos que nesitaremos
+    const userId = 1;
     const newDataUser = {
       name: "Mateito Amado",
       job: "SDTE",
     };
 
-    const response = await request.put("/users/1", { data: newDataUser });
+    // const response = await request.put("/users/1", { data: newDataUser });
+    const response = await userController.updateUser(userId, newDataUser);
     expect(response.status()).toBe(200);
 
     const responseBody: IResponseBodyJSONPlaceHolder = await response.json();
@@ -84,12 +96,71 @@ test.describe("Pruebas del backend", () => {
   test("TC-19: Validar eliminación de usuario mediante DELETE (API)", async ({
     request,
   }) => {
-    const response = await request.delete("/users/1");
+    // Instanciamos la clase UserContller para usar sus métodos.
+    const userController = new UserController(request);
+
+    const userId = 1;
+    const response = await userController.deleteUser(userId);
     expect(response.status()).toBe(200);
 
     const responseBody = await response.json();
     console.log("responseBody del DELETE", responseBody);
 
     expect(Object.keys(responseBody).length).toBe(0);
+  });
+
+  test("TC-20: Validar código 404 al buscar un usuario que NO existe (Edge Case)", async ({
+    request,
+  }) => {
+    // Instanciamos la clase UserContller para usar sus métodos.
+    const userController = new UserController(request);
+
+    const nonExistentUser = 99999;
+
+    const response = await userController.getUser(nonExistentUser);
+    // 404: Not found.
+    expect(response.status()).toBe(404);
+  });
+
+  test("TC-21: Validar código 401 Unauthorized por falta de password", async ({
+    request,
+  }) => {
+    // Nota: sobreescribimos temporalmente la Base url.
+    const invalidPayload = {
+      email: "sidney@fife",
+      // no enviamos password a proposito.
+    };
+    const response = await request.post("https://reqres.in/api/register", {
+      data: invalidPayload,
+    });
+
+    console.log("response", response);
+
+    // 401: Unauthorized
+    expect(response.status()).toBe(401);
+
+    const responseBody = await response.json();
+    console.log("responseBody", responseBody);
+
+    // Validamos que la respuesta nos haya avisado del error.
+    expect(responseBody).toHaveProperty("error");
+  });
+
+  test("TC-22: Filtrar usuario mediante Query Parameters", async ({
+    request,
+  }) => {
+    // Instanciamos la clase UserContller para usar sus métodos.
+    const userController = new UserController(request);
+    const targetEmail = "Sincere@april.biz";
+
+    const response = await userController.getUserByEmail(targetEmail);
+    expect(response.status()).toBe(200);
+
+    const responseBody = await response.json();
+    console.log("responseBody", responseBody);
+
+    // La API devuelve un array.
+    expect(responseBody.length).toBe(1);
+    expect(responseBody[0].email).toBe(targetEmail);
   });
 });
